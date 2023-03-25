2b635293acfd41f98f04ea94745f9105e8d698e4
Acevedo <juanesacevedop1101@gmail.com>
2023-03-25 10:15:22 -0500

    Se crea proyecto de Tienda DOM y JS

---
+++ baseDatos.js
@@ -0,0 +1,17 @@
+export let productos = [
+    {
+        nombre:"Dragon Ball Fighter Z",
+        precio:150000,
+        foto:"https://firebasestorage.googleapis.com/v0/b/tiendasabadojeap.appspot.com/o/foto1.jpg?alt=media&token=70821db3-ed9f-4c59-9b4c-0f30580a6415"
+    },
+    {
+        nombre:"God Of War: Ragnarok",
+        precio:200000,
+        foto:"https://firebasestorage.googleapis.com/v0/b/tiendasabadojeap.appspot.com/o/foto3.jpg?alt=media&token=fa39ba6c-351d-4967-b1e6-7fde8c7f2f8c"
+    },
+    {
+        nombre:"Assasins Creed",
+        precio:250000,
+        foto:"https://firebasestorage.googleapis.com/v0/b/tiendasabadojeap.appspot.com/o/foto2.jpg?alt=media&token=be84d3f3-4d25-41b9-83c2-bfc4cc4d69a8"
+    }
+]
---
+++ controlador.js
@@ -0,0 +1,24 @@
+import {productos} from "./baseDatos.js"
+
+
+let fila=document.getElementById("fila")
+
+//Recorriendo un arreglo
+productos.forEach(function(producto){
+
+    let columna=document.createElement("div")
+    columna.classList.add("col")
+
+    let tarjeta=document.createElement("div")
+    tarjeta.classList.add("card","shadow")
+
+    let foto=document.createElement("img")
+    foto.classList.add("img-fluid")
+    foto.src=producto.foto
+
+    //padres e hijos
+    tarjeta.appendChild(foto)
+    columna.appendChild(tarjeta)
+    fila.appendChild(columna)
+
+})
